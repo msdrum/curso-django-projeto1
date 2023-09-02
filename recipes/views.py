d08dcc5a2f88c9aa8e92372087d65a10b38fa7fa
Marcio Silva <marciosilvadrum@gmail.com>
2023-09-02 00:35:43 -0300

    removing contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,6 @@
 # para renderizar os templates que estão dentro do app
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html')
