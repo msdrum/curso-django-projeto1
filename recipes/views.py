9eaffeb343f7af4cbfb7a8c6e86143fa8b594034
Marcio Silva <marciosilvadrum@gmail.com>
2023-09-17 17:16:20 -0300

    populating the main card with an fake lib of information, just for test

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,15 @@
 # para renderizar os templates que estão dentro do app
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(9)],
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
